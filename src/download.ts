584944dab6aceaca568e12814db90a8fa11de5eb
Sam Teale <85887214+SamTealeDrest@users.noreply.github.com>
2025-02-27 22:53:59 +0000

    fix: lint (#329)

--- src/download.ts
+++ src/download.ts
@@ -1,8 +1,7 @@
 import { mkdtempSync } from 'fs'
 import { tmpdir } from 'os'
 import { sep } from 'path'
-import {DefaultArtifactClient} from '@actions/artifact'
-
+import { DefaultArtifactClient } from '@actions/artifact'
 
 export async function download(name: string): Promise<string> {
   const dir = mkdtempSync(`${tmpdir()}${sep}`)
@@ -10,17 +9,17 @@ export async function download(name: string): Promise<string> {
 
   // Fetch list of artifacts for the current workflow run
   const artifactResponse = await artifact.listArtifacts()
-  const artifacts = artifactResponse.artifacts
+  const { artifacts } = artifactResponse
 
   // Find artifact ID based on name
-  const matchingArtifact = artifacts.find(a => a.name === name)
+  const matchingArtifact = artifacts.find((a) => a.name === name)
   if (!matchingArtifact) {
     throw new Error(`Artifact with name "${name}" not found`)
   }
 
   // Download using the artifact ID
   await artifact.downloadArtifact(matchingArtifact.id, {
-    path: dir
+    path: dir,
   })
 
   return dir
