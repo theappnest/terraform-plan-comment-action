fcce3243e25003652bf097bc7a0f075f2b89e66d
Sam Teale <85887214+SamTealeDrest@users.noreply.github.com>
2025-02-27 22:50:48 +0000

    fix: new library needs different client type (#328)

--- src/download.ts
+++ src/download.ts
@@ -1,11 +1,27 @@
 import { mkdtempSync } from 'fs'
 import { tmpdir } from 'os'
 import { sep } from 'path'
-import { create } from '@actions/artifact'
+import {DefaultArtifactClient} from '@actions/artifact'
+
 
 export async function download(name: string): Promise<string> {
   const dir = mkdtempSync(`${tmpdir()}${sep}`)
-  const client = create()
-  await client.downloadArtifact(name, dir)
+  const artifact = new DefaultArtifactClient()
+
+  // Fetch list of artifacts for the current workflow run
+  const artifactResponse = await artifact.listArtifacts()
+  const artifacts = artifactResponse.artifacts
+
+  // Find artifact ID based on name
+  const matchingArtifact = artifacts.find(a => a.name === name)
+  if (!matchingArtifact) {
+    throw new Error(`Artifact with name "${name}" not found`)
+  }
+
+  // Download using the artifact ID
+  await artifact.downloadArtifact(matchingArtifact.id, {
+    path: dir
+  })
+
   return dir
 }
